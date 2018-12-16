c97d98f24775b201c0c16ea7d7128137ec174845
Yaroslav <contact@yaroslavps.com>
2018-12-16 21:15:31 +0300

    small changes 16.12.2018

--- .scripts/yps_vars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-export YPS_SECRETKEY="6!_xghc+yc0)ch^us7f=flczc&c&ql_+uqqxy2o+*4d(mo!^!6"
-export YPS_DEBUG="true"
-export YPS_DBPASSWORD="elephantitis"
-export YPS_DBUSER="postgres"
-export YPS_HOST="localhost"
