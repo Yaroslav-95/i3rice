be58254f72db33986d2b83075cbdcfd56b0a2651
Yaroslav <contact@yaroslavps.com>
2019-01-14 02:17:40 +0300

    arch rice

---
+++ arch/install.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+sudo pacman -Sy networkmanager network-manager-applet networkmanager-openvpn nvim dbus i3-gaps tmux dmenu compton mupdf ranger mopidy ncmpcpp mpc mpv mplayer dunst cronie feh scrot ffmpeg w3m xcape youtube-dl newsboat wget curl arandr firefox chromium gimp inkscape xclip xorg gcc make fontconfig pkg-config lxappearance papirus-icon-theme alsa-utils imagemagick ntfs-3g unzip gnome-themes-standard gtk-engine-murrine telegram-desktop qt5ct r udisks2 xdotool xssstate sxiv
+
+sudo systemctl enable NetworkManager
