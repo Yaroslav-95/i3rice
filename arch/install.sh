ee999bdc1ec62ecdf8f7e43beadc6247ce21756c
Yaroslav <contact@yaroslavps.com>
2019-04-26 05:53:15 +0300

    dmenu colors

--- arch/install.sh
+++ arch/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo pacman -Syy networkmanager network-manager-applet networkmanager-openvpn neovim dbus i3-gaps tmux htop dmenu compton mupdf ranger mopidy ncmpcpp mpc mpv mplayer dunst cronie feh scrot ffmpeg w3m xcape youtube-dl newsboat wget curl arandr firefox chromium gimp inkscape xclip xorg gcc make fontconfig pkg-config lxappearance papirus-icon-theme alsa-utils imagemagick ntfs-3g unzip gnome-themes-standard gtk-engine-murrine telegram-desktop qt5ct r udisks2 xdotool xssstate sxiv perl-term-readline-gnu transmission-cli python-neovim python2-neovim python-pip calcurse acpi acpid powerline powerline-vim powerline-fonts python-pygit2
+sudo pacman -Syy networkmanager network-manager-applet networkmanager-openvpn neovim dbus i3-gaps tmux htop dmenu compton mupdf ranger mopidy ncmpcpp mpc mpv mplayer dunst cronie feh scrot ffmpeg w3m xcape youtube-dl newsboat wget curl arandr firefox chromium gimp inkscape xclip xorg gcc make fontconfig pkg-config lxappearance papirus-icon-theme alsa-utils imagemagick ntfs-3g unzip gnome-themes-standard gtk-engine-murrine telegram-desktop qt5ct r udisks2 xdotool xssstate sxiv perl-term-readline-gnu transmission-cli python-neovim python2-neovim python-pip calcurse acpi acpid powerline powerline-vim powerline-fonts python-pygit2 polkit polkit-gnome
 
 #yay ttf-symbola
 
