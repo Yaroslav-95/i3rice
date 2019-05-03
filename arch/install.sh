4b26c2fca5b83d0433fba2ff737ad9ee966c0314
Yaroslav <contact@yaroslavps.com>
2019-05-04 01:13:41 +0300

    some cron scripts

--- arch/install.sh
+++ arch/install.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-sudo pacman -Syy networkmanager network-manager-applet networkmanager-openvpn neovim dbus i3-gaps tmux htop dmenu compton mupdf ranger mopidy ncmpcpp mpc mpv mplayer dunst cronie feh scrot slop maim ffmpeg w3m xcape youtube-dl newsboat wget curl arandr firefox chromium gimp inkscape xclip xorg gcc make fontconfig pkg-config lxappearance papirus-icon-theme alsa-utils imagemagick ntfs-3g unzip unrar gnome-themes-standard gtk-engine-murrine telegram-desktop qt5ct r udisks2 xdotool xssstate sxiv perl-term-readline-gnu transmission-cli python-neovim python2-neovim python-pip calcurse acpi acpid powerline powerline-vim powerline-fonts python-pygit2 polkit polkit-gnome zathura zathura-pdf-mupdf zathura-djvu gst-libav vifm ffmpegthumbnailer
+sudo pacman -Syy networkmanager network-manager-applet networkmanager-openvpn neovim dbus i3-gaps tmux htop dmenu compton mupdf ranger mopidy ncmpcpp mpc mpv mplayer dunst cronie feh scrot slop maim ffmpeg w3m xcape youtube-dl newsboat wget curl arandr firefox chromium gimp inkscape xclip xorg gcc make fontconfig pkg-config lxappearance papirus-icon-theme alsa-utils imagemagick ntfs-3g unzip unrar gnome-themes-standard gtk-engine-murrine telegram-desktop qt5ct r udisks2 xdotool xssstate sxiv perl-term-readline-gnu transmission-cli python-neovim python2-neovim python-pip calcurse acpi acpid gnupg powerline powerline-vim powerline-fonts python-pygit2 polkit polkit-gnome zathura zathura-pdf-mupdf zathura-djvu gst-libav vifm ffmpegthumbnailer
 
-#yay ttf-symbola cli-visualizer compton-tryone-git stig python-ueberzug
+#yay ttf-symbola cli-visualizer compton-tryone-git stig python-ueberzug pam-gnupg mutt-wizard-git
 
 # Laptop
 #sudo pacman -S xf86-video-intel
