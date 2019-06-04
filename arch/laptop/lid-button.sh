78834ae69d279a188021f4ec0f70c18abc6e832a
Yaroslav <contact@yaroslavps.com>
2019-06-04 16:44:26 +0300

    acpi just suspend if i3 not running

--- arch/laptop/lid-button.sh
+++ arch/laptop/lid-button.sh
@@ -4,6 +4,8 @@ case "$3" in
         logger "LID close on custom";
         if [ "$(ps cax | grep i3)" ]; then
             su yaroslav -c "DISPLAY=:0 i3 exec 'i3session suspend'"
+        else
+            systemctl suspend
         fi
 		;;
 	open)
